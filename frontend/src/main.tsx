73f07634d743504f9a87492286c1d19196178494
Neitron <nweris@yandex.ru>
2023-05-13 20:50:52 +0300

    feat: add basic ci/cd

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./app/theme";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "@/shared/assets/scss/index.scss";
-import ErrorPage from "./pages/Error";
-
-
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- frontend/src/widgets/SIgnInForm/SignInForm.tsx
+++ frontend/src/widgets/SIgnInForm/SignInForm.tsx
@@ -1,52 +1,84 @@
-import { ViewOffIcon, ViewIcon } from '@chakra-ui/icons';
-import { Avatar, Box, Button, Heading, IconButton, Input, InputGroup, InputRightElement, Stack, Text, VStack, Link, Container } from '@chakra-ui/react';
-import { useState } from 'react';
-
+import { ViewOffIcon, ViewIcon } from "@chakra-ui/icons";
+import {
+  Avatar,
+  Box,
+  Button,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Text,
+  VStack,
+  Link,
+  Container,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const SignInForm = () => {
-	const [show, setShow] = useState(false)
-	const handleshowChange = (): void => setShow(!show)
+  const [show, setShow] = useState(false);
+  const handleshowChange = (): void => setShow(!show);
 
-
-	return (
-		<Box >
-			<Container sx={{ p: '30px' }} border={'1px solid #000'} maxW='sm' color='black'>
-				<VStack>
-					<Stack>
-						<Avatar
-							size='lg'
-							name={''}
-						/>{' '}
-					</Stack>
-					<Stack >
-						<Heading sx={{ m: '15px 0 25px' }} as={'h2'} size={'md'} >
-							Вход в личный кабинет
-						</Heading>
-					</Stack>
-				</VStack>
-				<Stack spacing={'1rem'} >
-					<Input size={'lg'} type='email' placeholder={'Email'} variant={'outline'} isRequired={true} />
-					<InputGroup size={'lg'}>
-						<Input type={show ? 'text' : 'password'} placeholder={'Пароль'} variant={'outline'} isRequired={true} />
-						<InputRightElement  >
-							<IconButton background={'inherit'} icon={show ? <ViewOffIcon /> : <ViewIcon />} onClick={handleshowChange} aria-label={'Search database'}></IconButton>
-						</InputRightElement>
-					</InputGroup>
-					<Button colorScheme={'orange'} size={'lg'}>Войти</Button>
-				</Stack>
-				<VStack sx={{ mt: '10px' }} >
-					<Stack>
-						<Text>
-							Нет аккаунта?
-						</Text>
-					</Stack>
-					<Stack>
-						<Link color='blue.600' href='#'>Зарегестрироваться</Link>
-					</Stack>
-				</VStack>
-			</Container>
-		</Box >
-	);
+  return (
+    <Box>
+      <Container
+        sx={{ p: "30px" }}
+        border={"1px solid #000"}
+        maxW="sm"
+        color="black"
+      >
+        <VStack>
+          <Stack>
+            <Avatar size="lg" name={""} />{" "}
+          </Stack>
+          <Stack>
+            <Heading sx={{ m: "15px 0 25px" }} as={"h2"} size={"md"}>
+              Вход в личный кабинет
+            </Heading>
+          </Stack>
+        </VStack>
+        <Stack spacing={"1rem"}>
+          <Input
+            size={"lg"}
+            type="email"
+            placeholder={"Email"}
+            variant={"outline"}
+            isRequired={true}
+          />
+          <InputGroup size={"lg"}>
+            <Input
+              type={show ? "text" : "password"}
+              placeholder={"Пароль"}
+              variant={"outline"}
+              isRequired={true}
+            />
+            <InputRightElement>
+              <IconButton
+                background={"inherit"}
+                icon={show ? <ViewOffIcon /> : <ViewIcon />}
+                onClick={handleshowChange}
+                aria-label={"Search database"}
+              ></IconButton>
+            </InputRightElement>
+          </InputGroup>
+          <Button colorScheme={"orange"} size={"lg"}>
+            Войти
+          </Button>
+        </Stack>
+        <VStack sx={{ mt: "10px" }}>
+          <Stack>
+            <Text>Нет аккаунта?</Text>
+          </Stack>
+          <Stack>
+            <Link color="blue.600" href="#">
+              Зарегестрироваться
+            </Link>
+          </Stack>
+        </VStack>
+      </Container>
+    </Box>
+  );
 };
 
 export default SignInForm;
