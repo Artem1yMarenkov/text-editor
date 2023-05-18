75da513766532b50c8e70f419a208831221c66ce
Neitron <nweris@yandex.ru>
2023-05-18 17:54:45 +0300

    feat: add interceptor for rotten token

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -1,31 +1,44 @@
 import axios from "axios";
 import { setLoading } from "./loading";
+import { setIsLogin } from "../../entities/Auth";
+import { setUserLoginResponseStatus } from "../../widgets/SIgnInForm/store";
 
 export const api = axios.create({
-	baseURL: "http://localhost/",
+  baseURL: "http://localhost/",
 });
 
-api.interceptors.request.use((config) => {
-	const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-	if (token) {
-		config["headers"]["Authorization"] = "Bearer " + token;
-	}
+    if (token) {
+      config["headers"]["Authorization"] = "Bearer " + token;
+    }
 
-	setLoading(true);
+    setLoading(true);
 
-	return config;
-}, (error) => {
-	setLoading(false);
-	return Promise.reject(error);
-});
+    return config;
+  },
+  (error) => {
+    setLoading(false);
+    return Promise.reject(error);
+  }
+);
 
-api.interceptors.response.use((response) => {
-	setLoading(false);
+api.interceptors.response.use(
+  (response) => {
+    setLoading(false);
 
-	return response;
-}, (error) => {
-	setLoading(false);
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.removeItem("token");
+      setIsLogin(false);
+      setUserLoginResponseStatus(401);
+    }
+    setLoading(false);
 
-	return Promise.reject(error);
-});
+    return Promise.reject(error);
+  }
+);
