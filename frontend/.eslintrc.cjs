af720ec8bc448560f3fa33b5bda48bf4c4af37ed
Dmitii <ponomarevdima047@gmail.com>
2023-05-19 21:25:29 +0300

    fix:eslint setting

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,14 +1,27 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		"plugin:import/recommended",
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'airbnb',
+		'airbnb-typescript',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest', sourceType: 'module', project: true,
+		tsconfigRootDir: './tsconfig.json'
+	},
+	plugins: ['react-refresh', '@typescript-eslint', "import"],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		"@typescript-eslint/quotes": "off",
+		'@typescript-eslint/indent': 'off',
+		'no-tabs': 'off',
+		'no-trailing-spaces': 'off',
+		'linebreak-style': 'off',
+		'eol-last': 'off'
+	},
 }
