f079ec23b3ac82304e5d18955f3f8153f6aa0e0a
n1blxck <nestea324@mail.ru>
2023-06-06 22:27:24 +0300

    TEX-39: add icons for sidebar

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,54 @@
 import cors from '@fastify/cors'
 import fastify from "fastify";
 import { dbInstance } from './database';
+import { userRouter } from './modules/user/user.route';
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
 
-const instance = fastify({
+const app = fastify({
 	logger: true
 });
 
-instance.register(cors, {
-	origin: (_, callback) => {
-		callback(null, true);
-	}
-});
-
-instance.addHook("onRequest", (req, res, done) => {
-	req.headers['Content-Type'] = 'application/json';
-	done();
-})
 
 async function bootstrap() {
 	const config = {
 		port: 5500,
 		host: "0.0.0.0"
 	};
+
+	await app.register(swagger, {
+		swagger: {
+			info: {
+				version: "0.0.1",
+				title: "TextEditor REST API Documentation"
+			},
+		}
+	});
+
+	await app.register(swaggerUi, { routePrefix: "/docs" })
+
+	app.register(cors, {
+		origin: (_, callback) => {
+			callback(null, true);
+		}
+	});
+	
+	app.register(userRouter, { prefix: "/user" });
+
+	
+	app.addHook("onRequest", (req, res, done) => {
+		req.headers['Content-Type'] = 'application/json';
+		done();
+	});
+
+	app.ready(err => {
+		if (err) throw err;
+		app.swagger();
+	})
 	
 	try {
 		console.log(dbInstance);
-		instance.listen(config);
+		app.listen(config);
 	} catch {
 		console.error(`Failed to listen port ${config.port}`);
 		process.exit(1);
