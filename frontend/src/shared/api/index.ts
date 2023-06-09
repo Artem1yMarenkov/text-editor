f5e224102af8350f0a80b34a84297538e0e0744f
Artem1yMarenkov <orvysegor@gmail.com>
2023-06-09 13:12:52 +0300

    fix: interceptor headers token

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -11,7 +11,7 @@ api.interceptors.request.use(
     const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization = `Bearer ${"  "} ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     setLoading(true);
